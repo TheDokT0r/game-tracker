439e701ca91d3887c7b3186094c32154825bd3e9
Or Kwitzel <orkv1234@gmail.com>
2024-10-19 02:14:07 +0300

    fixed scss compiler errors

--- services/frontend/vite.config.ts
+++ services/frontend/vite.config.ts
@@ -10,4 +10,12 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+      },
+    },
+  },
 });
